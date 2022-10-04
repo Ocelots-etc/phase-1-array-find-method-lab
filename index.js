55625bea54648cd48f39fb85d772dfecf3cba57f
catdgomez <catherinedgomez@gmail.com>
2022-10-03 21:22:20 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // code your solution here
 function superbowlWin(arrayOfGames){
-  arrayOfGames.find(({result}) => result === "W")
+
+  for (let gameInfo of arrayOfGames){
+    if (gameInfo.result === "W") {
+      return gameInfo.year
+    }
+    arrayOfGames.find(({result}) => result === "W")
+  }
 }
